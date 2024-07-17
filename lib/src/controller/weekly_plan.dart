56a24decfb064fa84d8f379089f872c74c491446
long nguyen <longlop68@gmail.com>
2024-07-17 21:35:17 +0700

    g

--- lib/src/controller/weekly_plan.dart
+++ lib/src/controller/weekly_plan.dart
@@ -1,6 +1,5 @@
 import 'dart:convert';
 import 'package:it_can_cook/src/api/rest.dart';
-import 'package:it_can_cook/src/models/account.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class WeeklyPlanController {
