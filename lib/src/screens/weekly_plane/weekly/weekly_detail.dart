1b41377dd8137318ea90cfe2c51b1b21ea05a928
long nguyen <longlop68@gmail.com>
2024-08-22 08:49:39 +0700

    Update WeeklyDetailPage to use TabBar and TabBarView for day selection

--- lib/src/screens/weekly_plane/weekly/weekly_detail.dart
+++ lib/src/screens/weekly_plane/weekly/weekly_detail.dart
@@ -21,9 +21,20 @@ class WeeklyDetailPage extends StatefulWidget {
   WeeklyDetailPageState createState() => WeeklyDetailPageState();
 }
 
-class WeeklyDetailPageState extends State<WeeklyDetailPage> {
+class WeeklyDetailPageState extends State<WeeklyDetailPage>
+    with TickerProviderStateMixin {
+  late TabController tabController;
   //state //selected date
-  int _selectedValue = 1;
+
+  @override
+  void initState() {
+    super.initState();
+    tabController = TabController(
+      initialIndex: 0,
+      length: 7,
+      vsync: this,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -37,11 +48,7 @@ class WeeklyDetailPageState extends State<WeeklyDetailPage> {
     }
 
     return BlocListener<TriggerBloc, Trigger>(
-        listener: (context, state) {
-          setState(() {
-            _selectedValue = _selectedValue;
-          });
-        },
+        listener: (context, state) {},
         child: LoaderOverlay(
           child: Scaffold(
             appBar: AppBar(
@@ -218,87 +225,74 @@ class WeeklyDetailPageState extends State<WeeklyDetailPage> {
             body: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Container(
-                  padding: const EdgeInsets.only(right: 10, left: 10),
-                  height: 160,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(
-                        """ ${S.of(context).week} ${getCurrentWeekInMonth(DateTime.now().add(const Duration(days: 7)))}, ${DateFormat('MMMM').format(DateTime.now().add(const Duration(days: 7)))}, ${DateTime.now().add(Duration(days: DateTime.monday - DateTime.now().weekday + 7)).year}""",
-                        // get moth name
-
-                        style: const TextStyle(
-                          fontSize: 24,
-                          fontWeight: FontWeight.bold,
-                          shadows: [
-                            Shadow(
-                              blurRadius: 1.0,
-                              color: Colors.black,
-                              offset: Offset(1.0, 1.0),
-                            ),
-                          ],
-                        ),
+                TabBar(
+                  controller: tabController,
+                  tabs: [
+                    Tab(text: S.current.monday),
+                    Tab(text: S.current.tuesday),
+                    Tab(text: S.current.wednesday),
+                    Tab(text: S.current.thursday),
+                    Tab(text: S.current.friday),
+                    Tab(text: S.current.saturday),
+                    Tab(text: S.current.sunday),
+                  ],
+                ),
+                Expanded(
+                  child: TabBarView(
+                    controller: tabController,
+                    children: [
+                      Part(
+                        dayInWeek: 1,
+                        weeklyPlanId: widget.weeklyPlan.id!,
+                        recipes: widget.weeklyPlan.recipePlans
+                            .where((element) => element.dayInWeek == 1)
+                            .toList(),
                       ),
-                      const SizedBox(
-                        height: 10,
+                      Part(
+                        dayInWeek: 2,
+                        weeklyPlanId: widget.weeklyPlan.id!,
+                        recipes: widget.weeklyPlan.recipePlans
+                            .where((element) => element.dayInWeek == 2)
+                            .toList(),
                       ),
-                      DatePicker(
-                        height: 100,
-                        width: 75,
-                        daysCount: 7,
-                        locale: //get current locale
-                            Localizations.localeOf(context).languageCode,
-                        DateTime.now().add(Duration(
-                            days:
-                                DateTime.monday - DateTime.now().weekday + 7)),
-                        initialSelectedDate: DateTime.now().add(Duration(
-                            days:
-                                DateTime.monday - DateTime.now().weekday + 7)),
-                        selectionColor:
-                            const Color(0xFF02335A).withOpacity(0.8),
-                        selectedTextColor: Colors.white,
-                        dayTextStyle: TextStyle(
-                            fontWeight: FontWeight.w500,
-                            color:
-                                Theme.of(context).brightness == Brightness.dark
-                                    ? Colors.white
-                                    : Colors.black),
-                        monthTextStyle: TextStyle(
-                            fontWeight: FontWeight.w500,
-                            color:
-                                Theme.of(context).brightness == Brightness.dark
-                                    ? Colors.white
-                                    : Colors.black),
-                        dateTextStyle: TextStyle(
-                            fontWeight: FontWeight.w500,
-                            color:
-                                Theme.of(context).brightness == Brightness.dark
-                                    ? Colors.white
-                                    : Colors.black),
-                        onDateChange: (date) {
-                          setState(() {
-                            _selectedValue = date.weekday;
-                          });
-                        },
+                      Part(
+                        dayInWeek: 3,
+                        weeklyPlanId: widget.weeklyPlan.id!,
+                        recipes: widget.weeklyPlan.recipePlans
+                            .where((element) => element.dayInWeek == 3)
+                            .toList(),
+                      ),
+                      Part(
+                        dayInWeek: 4,
+                        weeklyPlanId: widget.weeklyPlan.id!,
+                        recipes: widget.weeklyPlan.recipePlans
+                            .where((element) => element.dayInWeek == 4)
+                            .toList(),
+                      ),
+                      Part(
+                        dayInWeek: 5,
+                        weeklyPlanId: widget.weeklyPlan.id!,
+                        recipes: widget.weeklyPlan.recipePlans
+                            .where((element) => element.dayInWeek == 5)
+                            .toList(),
+                      ),
+                      Part(
+                        dayInWeek: 6,
+                        weeklyPlanId: widget.weeklyPlan.id!,
+                        recipes: widget.weeklyPlan.recipePlans
+                            .where((element) => element.dayInWeek == 6)
+                            .toList(),
+                      ),
+                      Part(
+                        dayInWeek: 7,
+                        weeklyPlanId: widget.weeklyPlan.id!,
+                        recipes: widget.weeklyPlan.recipePlans
+                            .where((element) => element.dayInWeek == 7)
+                            .toList(),
                       ),
                     ],
                   ),
                 ),
-                SizedBox(
-                  height: MediaQuery.of(context).size.height - 380,
-                  child: SingleChildScrollView(
-                    child: Part(
-                      dayInWeek: _selectedValue,
-                      weeklyPlanId: widget.weeklyPlan.id!,
-                      recipes: widget.weeklyPlan.recipePlans
-                          .where(
-                              (element) => element.dayInWeek == _selectedValue)
-                          .toList(),
-                    ),
-                  ),
-                )
               ],
             ),
           ),
